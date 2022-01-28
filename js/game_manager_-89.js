b963da744038af8b6fc8599902a9e07b46b72e8e
Bonzey2416 <98016194+Bonzey2416@users.noreply.github.com>
2022-01-28 11:55:12 +0000

    Update game_manager_-89.js

--- js/game_manager_-89.js
+++ js/game_manager_-89.js
@@ -135,6 +135,7 @@ GameManager.prototype.canMerge = function (cellValue1, cellValue2) {
   // Use heuristics rather than a lookup, because... it's fun.
   if (cellValue1 === 1 && cellValue2 === 1) return true;
   if (cellValue1 === -1 && cellValue2 === -1) return true;
+  if (cellValue1 === -2 && cellValue2 === -1) return true;
   if (cellValue1 > cellValue2) {
      if (cellValue1 <= cellValue2 * 2) return true;
   } else if (cellValue2 > cellValue1) {
